28930f4684d47f92c129a1802ac86431fdc04e66
flo <fmaury.dev@free.fr>
2022-08-30 10:49:55 +0200

    modif calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
-const add = (a, b) => { return a + b };
-const sub = (a, b) => { return a - b };
-const multi = (a, b) => { return a * b };
-const div = (a, b) => { return a / b };
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multi = (a, b) => a * b;
+const div = function (a, b) { 
+    if (a == 0 && b == 0) {
+        return undefined;
+    }  
+    return a / b;
+};
 
 const operators = {"+" : add, "-" : sub, "*" : multi, "/" : div};
 
@@ -14,15 +19,21 @@ do {
     }
 } while (!operators.hasOwnProperty(operator));
 
-let nb1 = parseFloat(prompt("Première opérande"));
-let nb2 = parseFloat(prompt("Seconde opérande"));
 
-try {
-    let result = operators[operator](nb1, nb2);
-    if (isNaN(result)) {
-        throw new Error("Erreur de saisie. Nombres attendus.")
+let nb1, nb2, resultError;
+do {
+    nb1 = parseFloat(prompt("Première opérande"));
+    nb2 = parseFloat(prompt("Seconde opérande"));
+
+    try {
+        let result = operators[operator](nb1, nb2);
+        if (isNaN(result) && result !== undefined) {
+            throw new Error("Erreur de saisie. Nombres attendus.")
+        }
+        resultError = false;
+        alert(`${nb1} ${operator} ${nb2} = ${operators[operator](nb1, nb2)}`);
+    } catch (e) {
+        resultError = true;
+        alert(e.message);
     }
-    alert(`${nb1} ${operator} ${nb2} = ${operators[operator](nb1, nb2)}`);
-} catch (e) {
-    alert(e.message);
-}
+} while (resultError);
