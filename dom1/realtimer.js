0b0a5b9e52c780930b9eb432825eeecddf686c73
flo <fmaury.dev@free.fr>
2022-08-31 16:29:22 +0200

    small modif realtimer

--- dom1/realtimer.js
+++ dom1/realtimer.js
@@ -1,32 +1,33 @@
-const MAX_SECONDS = 10;
-const startButton = document.querySelector("button");
-const h2 = document.querySelector("h2");
+(() => {
+    const MAX_SECONDS = 10;
+    const startButton = document.querySelector("button");
+    const h2 = document.querySelector("h2");
 
-var seconds = MAX_SECONDS;
+    let seconds = MAX_SECONDS;
+    let timer;
 
-let timer;
-
-function start() {
-    displayTime();
-    timer = setInterval(countdown, 1000);
-}
-
-function stop() {
-    clearInterval(timer);
-    timer = null;
-    seconds = MAX_SECONDS;
-}
-
-function displayTime() {
-    h2.textContent = seconds == 0 ? "" : seconds;
-}
-
-function countdown() {
-    seconds--;
-    displayTime();
-    if (seconds == 0) {
-        stop();
+    function start() {
+        displayTime();
+        timer = setInterval(countdown, 1000);
     }
-}
 
-startButton.addEventListener('click', start);
+    function stop() {
+        clearInterval(timer);
+        timer = null;
+        seconds = MAX_SECONDS;
+    }
+
+    function displayTime() {
+        h2.textContent = seconds == 0 ? "" : seconds;
+    }
+
+    function countdown() {
+        seconds--;
+        displayTime();
+        if (seconds == 0) {
+            stop();
+        }
+    }
+
+    startButton.addEventListener('click', start);
+})();
