e238823be770c0b9c2aab91aa3aaaec5574cb1fe
flo <fmaury.dev@free.fr>
2022-09-05 15:33:45 +0200

    objects: added date.js

---
+++ objects/date.js
@@ -0,0 +1,15 @@
+let date = new Date();
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns 
+ */
+function formatDateFR(date) {
+    const days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    return `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+console.log(formatDateFR(date));
+console.log(date.toLocaleDateString('fr-FR', {weekday:"long", day:"numeric", month:"long", year:"numeric"}).split(" ").map(e => e.charAt(0).toUpperCase() + e.substring(1)).join(" "));
