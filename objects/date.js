a59b01ef563023d81a1696dd4b7300f9daaef0b6
flo <fmaury.dev@free.fr>
2022-09-05 15:40:22 +0200

    objects: modif date.js

--- objects/date.js
+++ objects/date.js
@@ -11,5 +11,10 @@ function formatDateFR(date) {
     return `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 
+
+String.prototype.capitalize = function () {
+    return this.split(" ").map(e => e.charAt(0).toUpperCase() + e.substring(1)).join(" ");
+}
+
 console.log(formatDateFR(date));
-console.log(date.toLocaleDateString('fr-FR', {weekday:"long", day:"numeric", month:"long", year:"numeric"}).split(" ").map(e => e.charAt(0).toUpperCase() + e.substring(1)).join(" "));
+console.log(date.toLocaleDateString('fr-FR', {weekday:"long", day:"numeric", month:"long", year:"numeric"}).capitalize());
