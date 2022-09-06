bfae51c95fda560e8c9c6dff49c1a2d220721421
flo <fmaury.dev@free.fr>
2022-09-06 11:29:20 +0200

    picsum: added some buttons

--- fetch/picsum.js
+++ fetch/picsum.js
@@ -2,13 +2,20 @@ function getRandomInt(max, min=0) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-fetch(`https://picsum.photos/v2/list?page=${getRandomInt(51)}&limit=10`)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.table(data);
-    const mainContainer = document.querySelector('main');
+function main(url) {
+    fetch(url)
+    .then(function(response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.status);
+    })
+    .then(data => display(data))
+    .catch(e => alert(`Impossible d'afficher la page.\nCode erreur ${e.message}`));
+}
+
+function display(data) {
+    // console.table(data);
     data.forEach(function (e) {
         let itemContainer = document.createElement('div');
         let infoContainer = document.createElement('div');
@@ -27,4 +34,35 @@ fetch(`https://picsum.photos/v2/list?page=${getRandomInt(51)}&limit=10`)
         infoContainer.classList.add("info-container");
         itemContainer.classList.add("item-container");
     });
+}
+
+const mainContainer = document.querySelector('main');
+const btnReload = document.getElementById("reload");
+const btnNext = document.getElementById("next");
+let page, url;
+
+function setURL(page) {
+    url = `https://picsum.photos/v2/list?page=${page}&limit=10`;
+    console.log(`currURL = ${url}`);
+}
+
+function setRandomPageURL() {
+    page = getRandomInt(51, 1);
+    setURL(page);
+}
+
+setRandomPageURL();
+window.addEventListener('load', () => main(url));
+
+btnReload.addEventListener('click', function () {
+    mainContainer.replaceChildren();
+    setRandomPageURL();
+    window.dispatchEvent(new Event('load'));
 });
+
+btnNext.addEventListener('click', function () {
+    mainContainer.replaceChildren();
+    page = Math.max(1, (page + 1) % 51);
+    setURL(page);
+    window.dispatchEvent(new Event('load'));
+})
